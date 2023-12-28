8ca48fc179e5b730d45f3a07e2170014c6779204
UtsavBalar1231 <utsavbalar1231@gmail.com>
2023-12-28 11:39:17 +0530

    include: configs: rockchip: Set default boot targets order
    Change-Id: I8a63bdaa68ddc73ddc9546d9f433f88e7c53cce4
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- include/configs/rockchip-common.h
+++ include/configs/rockchip-common.h
@@ -13,7 +13,63 @@
 
 #ifndef CONFIG_SPL_BUILD
 
-#define BOOT_TARGETS	"mmc1 mmc0 nvme scsi usb pxe dhcp spi"
+#define BOOT_TARGETS	"usb mmc0 mmc1 nvme scsi pxe dhcp spi"
+
+#if CONFIG_IS_ENABLED(CMD_MMC)
+	#define BOOT_TARGET_MMC(func) \
+		func(MMC, mmc, 0) \
+		func(MMC, mmc, 1)
+#else
+	#define BOOT_TARGET_MMC(func)
+#endif
+
+#if CONFIG_IS_ENABLED(CMD_MTD_BLK)
+	#define BOOT_TARGET_MTD(func)	\
+		func(MTD, mtd, 2)	\
+		func(MTD, mtd, 1)	\
+		func(MTD, mtd, 0)
+#else
+	#define BOOT_TARGET_MTD(func)
+#endif
+
+#if CONFIG_IS_ENABLED(CMD_RKNAND)
+	#define BOOT_TARGET_RKNAND(func) func(RKNAND, rknand, 0)
+#else
+	#define BOOT_TARGET_RKNAND(func)
+#endif
+
+#if CONFIG_IS_ENABLED(CMD_NVME)
+	#define BOOT_TARGET_NVME(func) func(NVME, nvme, 0)
+#else
+	#define BOOT_TARGET_NVME(func)
+#endif
+
+#if CONFIG_IS_ENABLED(CMD_USB)
+	#define BOOT_TARGET_USB(func) func(USB, usb, 0)
+#else
+	#define BOOT_TARGET_USB(func)
+#endif
+
+#if CONFIG_IS_ENABLED(CMD_PXE)
+	#define BOOT_TARGET_PXE(func) func(PXE, pxe, na)
+#else
+	#define BOOT_TARGET_PXE(func)
+#endif
+
+#if CONFIG_IS_ENABLED(CMD_DHCP)
+	#define BOOT_TARGET_DHCP(func) func(DHCP, dhcp, na)
+#else
+	#define BOOT_TARGET_DHCP(func)
+#endif
+
+#define BOOT_TARGET_DEVICES(func) \
+	BOOT_TARGET_USB(func) \
+	BOOT_TARGET_MMC(func) \
+	BOOT_TARGET_NVME(func) \
+	BOOT_TARGET_MTD(func) \
+	BOOT_TARGET_RKNAND(func) \
+	BOOT_TARGET_PXE(func) \
+	BOOT_TARGET_DHCP(func)
 
 #ifdef CONFIG_ARM64
 #define ROOT_UUID "B921B045-1DF0-41C3-AF44-4C6F280D3FAE;\0"
